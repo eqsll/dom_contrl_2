1c4cd0ecce84558e6bc4ce33d5cf0a049a32eea4
eqsll <144461926+eqsll@users.noreply.github.com>
2023-12-04 14:54:00 +0300

    Add files via upload

---
+++ код задачи 1.pas
@@ -0,0 +1,23 @@
+﻿var a: array [1..10] of integer;
+    l, i: integer;
+    c: boolean;
+begin
+  for i:= 1 to 10 do
+    read (a[i]);
+  c:= True;
+  l:= 10;
+  i:= 1;
+  while c do begin
+  while (i+2 <= 10) and (c = True) do
+    begin
+    if a[i] + a[i + 1] <> a[i + 2] then begin
+    c:=False;
+    writeln ('Массив не соответствует последовательности Фибоначчи');
+    end;
+    i:= i + 1;
+    end;
+    if c = True then
+      c:=False;
+      writeln ('Массив cоответствует последовательности Фибоначчи');
+    end;
+end.
---
+++ код задачи 2.pas
@@ -0,0 +1,37 @@
+﻿var
+  s, s1, s2: string;
+  q, w, i, j, k, n, c: integer;
+
+begin
+  writeln('Введите строку содержащую несколько одинаковых подстрок');
+  readln(s);
+  writeln('Введите подстроку для поиска и удаления');
+  readln(s1);
+  q := length(s);
+  n := 0;
+  i := 1;
+  while i <= length(s) do
+    if s[i] = s1[1] then
+    begin
+      j := 1;
+      while (j <= length(s1)) and (s[i + j - 1] = s1[j]) do inc(j);
+      if j > length(s1) then inc(n);
+      if n mod 2 = 0 then
+      begin
+        w+=1;
+        for j := 1 to length(s1) do
+        begin
+          for k := i to length(s) - 1 do
+            s[k] := s[k + 1];
+          SetLength(s2, q-w);
+          for c:= 1 to q - w do
+          begin
+            s2[c]:= s[c];
+          end;
+        end;
+      end
+      else i := i + 1
+    end
+    else i := i + 1;
+  writeln(s2);
+end.
